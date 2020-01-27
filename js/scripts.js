373df2b889578193b7d0a437b48ee65a805544de
Brandan Sayarath and Zakkrey Short <brandan@>
2020-01-27 14:50:03 -0800

    rework functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,21 @@ vacations.prototype.addDetails = function(destination) {
   this.destinations.push(destination);
 }
 
+vacations.prototype.assignId = function(){
+  this.currentId += 1;
+  return this.currentId;
+}
+
+vacations.prototype.findDestination = function(id){
+  for (var i = 0; i < this.destinations.length; i++){
+    if(this.destinations[i]){
+      if(this.destinations[i].id == id){
+        return this.destinations[i];
+      }
+    }
+  };
+  return false;
+}
 
 //Constructor
 function destination(city, country, yearVisited) {
@@ -16,32 +31,44 @@ function destination(city, country, yearVisited) {
   this.country = country;
   this.yearVisited = yearVisited;
 }
-// show list funciton
-function showlist() {
-$("#result").append("<h1>" + newVacation.city + "</h1>");
-$("#result").click(function(){
-$("#tripDetails").append("<li>" + newVacation.country + " " + newVacation.yearVisited + "</li>");
-}) 
-}
+
 //User Logic
 
 var listOfTrips = new vacations();
+
+function displayDetails (destinationListToDisplay){
+  var myList = $("ul#tripDetails");
+  var details = "";
+  destinationListToDisplay.destinations.forEach(function(listItem){
+    details += "<li id=" + listItem.id + ">" + listItem.name + "</li>"
+  });
+  myList.html(details);
+}
+
+// function display
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     $("#tripDetails").text('');
-
+    
     var cityinput = $("input#city").val();
     var countryinput = $("input#country").val();
     var yearinput = $("input#year").val();
-
+    
     var newVacation = new destination(cityinput, countryinput, yearinput);
     listOfTrips.destinations.push(newVacation); 
-
-    console.log(listOfTrips.destinations);
-    listOfTrips.destinations.forEach(function(destination) {
-      $("#tripDetails").append('<p>' + destination.city + '</p>');
-    });
-  })
+    
+    displayDetails(listOfTrips);
+    console.log(listOfTrips);
+    
+  //   listOfTrips.destinations.forEach(function(destination) {
+  //     $("#tripDetails").append('<p>' + destination.city + '</p>');
+  //   });
+  // })
+  //     // for(i=0; i <= destination.length; i++)       
+  //     $("#tripDetails").click(function(){
+  //     $("#tripDetails").append("<li>" + listOfTrips.destinations[i].country + " " + listOfTrips.destinations[i].yearVisited + "</li>");
+  //     }) 
+  })           
 })
