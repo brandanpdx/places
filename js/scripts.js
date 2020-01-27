d84ca645349cd95abb8614b01d8ba01edc192ec9
Brandan Sayarath and Zakkrey Short <zakkrey@>
2020-01-27 13:43:28 -0800

    form returns output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //Business Logic for Vacations
 function vacations() {
   this.destinations = [];
+  this.currentId = 0;
 }
 
 vacations.prototype.addDetails = function(destination) {
+  destination.id = this.assignId();
   this.destinations.push(destination);
 }
+
 
 //Constructor
 function destination(city, country, yearVisited) {
@@ -13,7 +16,13 @@ function destination(city, country, yearVisited) {
   this.country = country;
   this.yearVisited = yearVisited;
 }
-
+// show list funciton
+function showlist() {
+$("#result").append("<h1>" + newVacation.city + "</h1>");
+$("#result").click(function(){
+$("#tripDetails").append("<li>" + newVacation.country + " " + newVacation.yearVisited + "</li>");
+}) 
+}
 //User Logic
 
 var listOfTrips = new vacations();
@@ -21,22 +30,18 @@ var listOfTrips = new vacations();
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    $("#tripDetails").text('');
+
     var cityinput = $("input#city").val();
     var countryinput = $("input#country").val();
     var yearinput = $("input#year").val();
 
     var newVacation = new destination(cityinput, countryinput, yearinput);
-    
     listOfTrips.destinations.push(newVacation); 
 
-      function showlist() {
-      $("#result").append("<h1>" + newVacation.city + "</h1>");
-      $("#result").click(function(){
-      $("#tripDetails").append("<li>" + newVacation.country + " " + newVacation.yearVisited + "</li>");
-      }) 
-    }
-    console.log(listOfTrips);
     console.log(listOfTrips.destinations);
-    listOfTrips.destinations.forEach(showlist);
+    listOfTrips.destinations.forEach(function(destination) {
+      $("#tripDetails").append('<p>' + destination.city + '</p>');
+    });
   })
 })
